32a9924f0a74566948871f66d15911b9e77662d6
InterGamesInt <119162206+InterGamesInt@users.noreply.github.com>
2022-12-13 14:03:37 +0200

    Add files via upload

---
+++ Session 2022.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cout << "Enter a natural number: " << '\n';
+	int a;
+	std::cin >> a;
+	if (a <= 0) {
+		std::cout<<"Wrong number!";
+		return 0;
+	}
+	std::cout << "Enter the n value: " << '\n';
+	int n;
+	std::cin >> n;
+	if (n == 0) {
+		std::cout << "Wrong number!";
+		return 0;
+	}
+	int res = 0;
+	for (int i = 1; i <= a; ++i) {
+		if (a % i == 0 && i % n == 0) {
+			res = res + 1;
+		}
+	}
+	std::cout << "The number " << a << " has " << res << " divisor fo multiples of " << n;
+}
